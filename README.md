b3baa2bb3e62514b83c36622f897837d3212b531
bigboss <bigboss@localhost.localdomain>
2017-05-31 16:54:32 -0700

    First commit. No actual functionality implemented. Just making so I can share this with the team

---
+++ README.md
@@ -0,0 +1,8 @@
+Bare-bones starting point for project. So far only toggles the GPIO pins PD12 and PD13.
+
+Put this in the Project Folder of the stm32\_discovery\_arm\_gcc\
+ie on my computer:
+  /home/src/bigboss/src/stm32\_discovery\_arm\_gcc/STM32F4-Discovery\_FW\_V1.1.0/Project/
+
+and for OSX:
+  /Users/bigboss/src/stm32\_discovery\_arm\_gcc/STM32F4-Discovery\_FW\_V1.1.0/Project/
---
+++ main.c
@@ -0,0 +1,49 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4_discovery.h"
+
+void Delay(__IO uint32_t nCount);
+void Init_GPIO();
+
+int main(void) {
+
+  /* GPIOD Periph clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+  Init_GPIO();
+
+  while (1) {
+    /* Turn on GPIO PD12, PD13 */
+    GPIO_SetBits(GPIOD, GPIO_Pin_12);
+    GPIO_SetBits(GPIOD, GPIO_Pin_13);
+
+    /* Insert delay */
+    Delay(0x3FFFFF);
+    
+    /* Turn off GPIO PD12, PD13 */
+    GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13);
+    
+    /* Insert delay */
+    Delay(0xFFFFFF);
+  }
+  return 0;
+}
+
+void Init_GPIO() {
+  /* Initialization struct */
+  GPIO_InitTypeDef  GPIO_InitStructure;
+
+  /* Configure PD12, PD13 */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+}
+
+void Delay(__IO uint32_t nCount)
+{
+  while(nCount--)
+  {
+  }
+}
