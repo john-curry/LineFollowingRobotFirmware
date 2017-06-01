bcbe9e057a3b6eb143711660e41dc77537e111b4
john-curry <liquidsnakeeater@gmail.com>
2017-06-01 10:54:48 -0700

    commented the robot_logic.h file

--- robot_logic.h
+++ robot_logic.h
@@ -3,8 +3,22 @@
 #include "robot.h"
 #include "ai.h"
 
+/*
+  Method declarations for logic defining hardware control.
+  This is where most of hardware programming should be done.
+  For now all methods should be NON-BLOCKING (ie no infinite while loops)
+  but this may change in future versions.
+*/
+
+/* 
+  Move robot forward my an incremental amount.
+  Turn on motors if they are not already on or keep them on.
+*/
 void move_Forward(Robot * robot, State * current_state);
 
+/*
+  Turn off motor or ensure that they are still off.
+*/
 void stop_Robot(Robot * robot, State * current_state);
 
 #endif
